74bd0610be5e4716c4a9c1aa17285d08cf3e1847
lucas <plebani.lucass@gmail.com>
2026-06-11 22:09:11 -0300

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,5 +1,147 @@
 const etapas = [
 
+    {
+        pergunta:"PERGUNTA 1",
+        resposta:"resposta1"
+    },
+
+    {
+        pergunta:"PERGUNTA 2",
+        resposta:"resposta2"
+    },
+
+    {
+        pergunta:"PERGUNTA 3",
+        resposta:"resposta3"
+    },
+
+    {
+        pergunta:"PERGUNTA 4",
+        resposta:"resposta4"
+    },
+
+    {
+        pergunta:"PERGUNTA 5",
+        resposta:"resposta5"
+    }
+
+];
+
+let etapaAtual = 0;
+
+const dots = document.querySelectorAll(".dot");
+const pergunta = document.getElementById("pergunta");
+const resposta = document.getElementById("resposta");
+const mensagem = document.getElementById("mensagem");
+
+function atualizarBolinhas(){
+
+    dots.forEach((dot,index)=>{
+
+        if(index<=etapaAtual){
+
+            dot.classList.add("ativo");
+
+        }else{
+
+            dot.classList.remove("ativo");
+
+        }
+
+    });
+
+}
+
+function carregar(){
+
+    atualizarBolinhas();
+
+    pergunta.style.opacity=0;
+
+    setTimeout(()=>{
+
+        pergunta.textContent=etapas[etapaAtual].pergunta;
+
+        pergunta.style.opacity=1;
+
+        resposta.value="";
+
+        mensagem.textContent="";
+
+        resposta.focus();
+
+    },180);
+
+}
+
+function verificar(){
+
+    let valor=resposta.value.trim().toLowerCase();
+
+    let correta=etapas[etapaAtual].resposta.toLowerCase();
+
+    if(valor===correta){
+
+        etapaAtual++;
+
+        if(etapaAtual>=etapas.length){
+
+            finalizar();
+
+        }else{
+
+            carregar();
+
+        }
+
+    }else{
+
+        mensagem.textContent="Tente novamente.";
+
+        resposta.value="";
+
+    }
+
+}
+
+function finalizar(){
+
+    document.querySelector(".info").style.display="none";
+
+    resposta.style.display="none";
+
+    mensagem.style.display="none";
+
+    atualizarBolinhas();
+
+    pergunta.innerHTML=`
+
+    Você chegou até aqui.
+
+    <br><br>
+
+    ♡
+
+    <br><br>
+
+    Seu presente está esperando.
+
+    `;
+
+}
+
+resposta.addEventListener("keydown",(e)=>{
+
+    if(e.key==="Enter"){
+
+        verificar();
+
+    }
+
+});
+
+carregar();const etapas = [
+
 {
 pergunta:"PERGUNTA 1",
 resposta:"resposta1"
